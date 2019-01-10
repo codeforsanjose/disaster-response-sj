c2f2cabbcc3ed2e06ba5d2995a093e264e724f5d
Joseph <jrichardson@jmstudios.net>
2019-01-09 21:36:41 -0800

    loading map of san jose in modal of post working

--- src/components/MapDisplay/MapDisplay.js
+++ src/components/MapDisplay/MapDisplay.js
@@ -5,12 +5,14 @@ import './MapDisplay.css'
 class MapDisplay extends Component {
     
     componentDidMount () {
+        const sanJoseLongitude = -121.8863
+        const sanJoseLatitude = 37.3382
         const map = new window.L.Map('map');
 
 	    var osmUrl='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
         var osmAttrib='Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors'
-        var osm = new window.L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 12, attribution: osmAttrib})
-        map.setView(new window.L.LatLng(51.3, 0.7),9)
+        var osm = new window.L.TileLayer(osmUrl, {minZoom: 4, maxZoom: 20, attribution: osmAttrib})
+        map.setView(new window.L.LatLng(sanJoseLatitude, sanJoseLongitude),11)
         map.addLayer(osm)
     }
 
@@ -19,7 +21,6 @@ class MapDisplay extends Component {
         
         return (
             <div className='MapDisplay'>
-            well map
                 <div id='map'>
                     
                 </div>
