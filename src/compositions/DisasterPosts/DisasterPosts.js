9fb6bb53a967942857adfa54692deb5967a865bf
Joseph <jrichardson@jmstudios.net>
2019-03-23 14:41:17 -0700

    remove commented out code

--- src/compositions/DisasterPosts/DisasterPosts.js
+++ src/compositions/DisasterPosts/DisasterPosts.js
@@ -5,64 +5,6 @@ import DisasterModalPostDetails from '../DisasterModalPostDetails/DisasterModalP
 
 import './DisasterPosts.css'
 
-// class DisasterPosts extends Component {
-//     state = {
-//         showModal: false,
-//         selectedPost: null,
-//     }
-
-//     openPostModal = (post) => {
-//         this.setState(previousState => {
-//             return {
-//                 ...previousState,
-//                 showModal: true,
-//                 selectedPost: post,
-//             }
-//         })
-//     }
-    
-//     postsMockup = (posts) => {
-//         return posts.map( (post, index) => {
-//             return <DisasterPostDetails post={post} keyIndex={index} openPostModal={this.openPostModal} />;
-//         })
-//     }
-
-//     dismissModal = (e) => {
-//         e.preventDefault()
-//         this.setState(prevState => {
-//             return {
-//                 ...prevState,
-//                 showModal: false,
-//                 selectedPost: null,
-//             }
-//         })
-//     }
-
-//     getModalDetails = () => {
-//         return <DisasterModalPostDetails
-//                 selectedPost={this.state.selectedPost}
-//                 handleSelectPost={this.props.handleSelectPost}
-//                 dismissModal={this.props.dismissModal} />
-//     }
-
-//     render() {
-//         const modalDetails = this.state.showModal ? this.getModalDetails() : null
-//         const backdrop = this.state.showModal ? <div className='backdrop' onClick={ this.dismissModal }></div> : null
-//         const posts = this.postsMockup(this.props.posts)
-//         return (
-//             <div className='DisasterPosts'>
-//                 { backdrop }
-//                 { modalDetails }
-//                 <section className='posts-list-container'>
-//                     { posts }
-//                 </section>
-//             </div>
-//         );
-//     }
-// }
-
-// export default DisasterPosts
-
 function DisasterPosts(props) {
     const defaultState = {
         showModal: false,
@@ -71,7 +13,7 @@ function DisasterPosts(props) {
     const [state, setState] = useState(defaultState)
 
     const openPostModal = (post) => {
-        this.setState(previousState => {
+        setState(previousState => {
             return {
                 ...previousState,
                 showModal: true,
