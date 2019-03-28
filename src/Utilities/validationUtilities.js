83da96ac3d3a4329b4fce3a462c98df74f24313f
Sunny Mui <sunnycmui@gmail.com>
2019-03-27 17:18:39 -0700

    setup basic structure of generalized validation function

--- src/Utilities/validationUtilities.js
+++ src/Utilities/validationUtilities.js
@@ -6,6 +6,50 @@ const validateEmail = (email, errorMessage) => {
     return errorMessage
 }
 
+const isInvalid = {
+  sanJoseRegionalPoints: {
+    maxLong: -118,
+    minLong: -124,
+    maxLat: 41,
+    minLat: 34,
+    maxRadius: 10
+  },
+  email: (email) => {
+    if ( email.length === 0 && validateEmail(email) ) {
+      return 'Invalid email, please re-enter valid email'
+    }
+  },
+  longitude: (long) => {
+    if ( sanJoseRegionalPoints.minLong > long || long > sanJoseRegionalPoints.maxLong ) {
+      return
+    }
+  },
+  latitude: (lat) => {
+    if () {
+
+    }
+  },
+  radius: (radius) => {
+    if () {
+
+    }
+  },
+  field: (fieldName, fieldInput) => {
+    if (adminState.postDetails[postField].length === 0 && (postField !== 'updates' && postField !== 'updateItem')) {
+      return 'Invalid ' + fieldName + '. Please re-enter a valid ' + fieldName
+    }
+  },
+  withDefault: function(validation, input) {
+    try {
+      return this[validation](input)
+    }
+    catch (e) {
+      return this.field(validation, input)
+    }
+  }
+}
+
 export {
     validateEmail
+    isInvalid
 }
