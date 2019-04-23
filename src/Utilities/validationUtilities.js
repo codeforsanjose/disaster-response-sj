323618d86db7d3cc426e86599a565bd7a3c8d113
Zain Qasmi <zainqasmi@gmail.com>
2019-04-22 19:12:24 -0700

    removed commented code

--- src/Utilities/validationUtilities.js
+++ src/Utilities/validationUtilities.js
@@ -1,5 +1,4 @@
 const validateEmail = (email, errorMessage) => {
-    //   eslint-disable-next-line
     let emailPatternReg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
     if (!emailPatternReg.test(email)) {
         errorMessage += 'Please enter a valid email\n'
