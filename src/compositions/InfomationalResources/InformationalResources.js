a6b3629cbf7b2b675b961647f0b1892a3372ae37
Sunny Mui <sunnycmui@gmail.com>
2019-04-26 02:14:39 -0700

    remove extra import from inforesources.js

--- src/compositions/InfomationalResources/InformationalResources.js
+++ src/compositions/InfomationalResources/InformationalResources.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react'
 
-
-import './InformationalResources.css'
 import './InformationalResources.js'
 import { infoResourcesMarkup } from './InformationalResourcesMarkup.js'
 
