ae50658695f604470edebec028550eba24f1c171
Joseph <jrichardson@jmstudios.net>
2019-07-08 19:01:11 -0700

    package-lock

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'normalize.css';
+
 import './index.css';
 import App from './containers/App/App';
 import registerServiceWorker from './registerServiceWorker';
