f257c6fbee0defd1f1c2d0754431dd9f415fbca8
Sunny Mui <sunnycmui@gmail.com>
2019-09-15 22:31:33 -0700

    refactor locationiq module for better module es6 practices, use template strings, named exports for modularity

--- src/services/locationiq.js
+++ src/services/locationiq.js
@@ -1,40 +1,35 @@
-const endpoints = {
+const KEY = process.env.REACT_APP_LOCATION_IQ_KEY;
+export const ENDPOINTS = {
 	forwardGeocode: 'https://us1.locationiq.com/v1/search.php?key=',
   reverseGeocode: 'https://us1.locationiq.com/v1/reverse.php?key='
-}
+};
 
-const encodeAddress = ({address, zip, city, state}) => {
+export const encodeAddress = ({address, zip, city, state}) => {
 	/*
-  Formats the address into a valid query string to append to the endpoint URL. Uses passed in address parts in an obj.
-  Args: address components (obj) - object containing address components formatted like so: {address: '123 fake st.', zip: '94560', city: 'San Jose', state: 'CA'}
-  Return: formatted address query (str) - address formatted as a query string
+  Formats address into a valid query string to append to the endpoint URL.
+  Args: address components (obj) - object containing address components formatted: {address: '123 fake st.', zip: '94560', city: 'San Jose', state: 'CA'}
+  Return: formatted address query (str)
   */
   // need to escape special characters in the address to be a valid URL string
   const encodedAddress = encodeURIComponent(`${address}, ${city}, ${state} ${zip}`);
 
-  return '&q=' +
-         encodedAddress +
-         '&format=json'
+  return `&q=${encodedAddress}&format=json`
 }
 
-const encodeLatLng = ({lat, lng}) => {
+export const encodeLatLng = ({lat, lng}) => {
 	/*
-  Formats the latitutde/longitude coordinates into a valid query string to append to the endpoint URL.
-  Args: latitude/longitude coordinates (obj) - object containing the latitude and longitude values
-  Return:
+  Formats latitude/longitude coordinates into a valid query string to append to the endpoint URL.
+  Args: latitude/longitude coordinates (obj) - object containing the latitude and longitude values: {lat: 37.12312, lng: -121.12314}
+  Return: formatted lat/lng query (str)
   */
-  return '&lat=' +
-      		lat +
-      		'&lon=' +
-      		lng +
-      		'&format=json';
+  return `&lat=${lat}&lon=${lng}&format=json`
 }
 
-const checkResponseJSON = (response) => {
+export const checkResponseJSON = (response) => {
   /*
   Checks the http response from fetch requests for errors. If it contains errors, it will reject the Promise with that relevant status codes.
   Args: http response (obj) - passed from a fetch function
-  Return: Success: json response (obj)
+  Return: Success: json response (obj) - api payload in json format
           Fail: Rejected Promise containing response status and status text (obj)
   */
   if (response.ok) {
@@ -48,9 +43,9 @@ const checkResponseJSON = (response) => {
   }
 }
 
-const checkAPIErrors = (JSON) => {
+export const checkAPIErrors = (JSON) => {
   /*
-  Checksfor an error returned from the API side ie unable to geocode a query or no results. Use after checking for http errors in the response because this just checks if the json response has a key with specifically 'error'
+  Checks for an error returned from the API side ie unable to geocode a query or no results. Use after checking for http errors in the response because this just checks if the json response has a key with specifically 'error'
   Args: json (obj) - the json object returned from response.json()
   Return: Success: json response (obj)
           Fail: Rejected Promise containing response status and status text (obj)
@@ -58,7 +53,7 @@ const checkAPIErrors = (JSON) => {
   if (JSON.error) {
     // to throw some error info back to the catch statement
     return Promise.reject({
-      status: 'The Geocoding API ran into an error while processing your search, please check that valid data was inputted and that the API is working correctly. This error happens if everything goes through, but the Geocoding API is unable to give you results for your search.',
+      status: 'The Geocoding API ran into an error while processing your search, please check that valid data was inputted and that the API is working correctly. This error happens if your search reaches the API server, but the Geocoding API is unable to give you results for your search.',
       statusText: JSON.error
     })
   } else {
@@ -66,14 +61,13 @@ const checkAPIErrors = (JSON) => {
   }
 }
 
-const get = async ({key, endpoint, encoder}, query) => {
+export const get = async ({key, endpoint, encoder}, query) => {
   /*
   Fetches a json response containing place information from the api based on the passed query and using the specified API key, endpoint URL, and encoder to format the query with.
   Args: settings (obj) - config obj containing the address strings to query aka street address, zip, city, and state
-  Return: Success - JSON response array of found coordinates (array) after first returning a promise obj
-  				Error - Error object containing status & statusText (obj)
+  Return: Success: JSON response array of found coordinates (array) after first returning a promise obj
+  				Error: Rejected Promise containing error response status and status text (obj)
   */
-
   // construct the URL to fetch
   const apiURL = endpoint + key + encoder(query);
 
@@ -91,19 +85,14 @@ const get = async ({key, endpoint, encoder}, query) => {
   }
 }
 
-const createClient = (key, endpoints) => {
-	return {
-  	get: {
-    	 LatLng: get.bind(this, {key: key,
-                               endpoint: endpoints.forwardGeocode,
-                               encoder: encodeAddress}),
-       Address: get.bind(this, {key: key,
-                               endpoint: endpoints.reverseGeocode,
-                               encoder: encodeLatLng})
-    }
-  }
+export const createGetter = (key, endpoint, encoder) => {
+  /*
+  Predefine api key, endpoint url, and query encoding function for a desired type of get request so you don't have to enter environment info every request.
+  Args: api key (str), endpoint url (str), query encoding function (obj)
+  Return: getter function (obj) - with preset args, except the query
+  */
+	return get.bind(this, {key, endpoint, encoder})
 }
 
-export default {
-  
-}
+export const getLatLng = createGetter(KEY, ENDPOINTS.forwardGeocode, encodeAddress);
+export const getAddress = createGetter(KEY, ENDPOINTS.reverseGeocode, encodeLatLng);
