caa6013151cf173a1cebfb7a8820e8d5a75f94f3
Sunny Mui <sunnycmui@gmail.com>
2020-03-05 22:49:01 -0800

    reinstall/rebuild dependncies to fix weird missing bcrypt module bug, update some dependencies to resolve security vulnerabilities, remove auth package due to critical unpatched security vulnerabilities and since package doesn't seem to be used anymore in project, fix win-all command to work properly in package.json, remove commented out code using auth package in userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -35,20 +35,6 @@ usersRouter.get('/api/user/:id', (req, res) => {
     .catch(error => {
         res.status(501).json({ error })
     })
-    //TODO Check why we need this. An Admin should be able to view anyone's profile
-    // if (req.user) {
-    //     var user = req.user
-    //     return res.json({ user });
-    // }
-    // if (auth.isAdmin(req) || (req.isAuthenticated() && req.params.id === req.user._id.toString())) {
-        // db.getById('user', req.params.id).then(user => {
-        //     delete user.passphrase
-        //     req.user = user
-        //     return res.json({ user })
-        // })
-    // } else {
-    //     return res.status(403).json({ error: 'You do not have permission to access this resource.' })
-    // }
 })
 
 const createNewUser = (newUser) => {
